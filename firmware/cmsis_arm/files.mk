f1e1af600182e6ec9321ca60d291bdf612b2ead5
Fabien-B <fabien.bonneval@gmail.com>
2020-01-25 21:04:03 +0100

    Adds part of CMSIS DSP.

---
+++ firmware/cmsis_arm/files.mk
@@ -0,0 +1,9 @@
+
+CSRC += $(DSPBASE)/arm_cos_f32.c
+CSRC += $(DSPBASE)/arm_sin_f32.c
+CSRC += $(DSPBASE)/arm_mat_mult_f32.c
+CSRC += $(DSPBASE)/arm_mat_add_f32.c
+CSRC += $(DSPBASE)/arm_mat_sub_f32.c
+CSRC += $(DSPBASE)/arm_mat_scale_f32.c
+CSRC += $(DSPBASE)/arm_common_tables.c
+
