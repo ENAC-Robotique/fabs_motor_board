ca00460d7a1a584b6b2df29c755bd8c32df0966d
Fabien-B <fabien.bonneval@gmail.com>
2021-11-08 20:53:19 +0100

    Test parse message.

--- test/test.py
+++ test/test.py
@@ -4,6 +4,7 @@ import struct
 import time
 from generated import coinlang_down_pb2 as cld, coinlang_up_pb2 as clu
 
+
 class Com:
 
     def __init__(self, port, baudrate=115200):
@@ -31,14 +32,40 @@ class Com:
         payload = msg.SerializeToString()
         self.send_data(payload)
 
+    def rcv(self):
+        state = 0
+        payload_len = 0
+        while True:
+            if state == 0:
+                if self.serial.read()[0] == 0xFF:
+                    state = 1
+                else:
+                    print(b)
+            if state == 1:
+                if self.serial.read()[0] == 0xFF:
+                    state = 2
+                else:
+                    state = 0
+            if state == 2:
+                payload_len = self.serial.read()[0]
+                payload = self.serial.read(payload_len)
+                chk = self.serial.read()[0]
+                if chk == self.compute_chk(payload):
+                    print("success")
+                    u = clu.UpMessage()
+                    u.ParseFromString(payload)
+                    if u.HasField("battery_report"):
+                        print(f"battery_report: {u.battery_report.voltage}")
+                    elif u.HasField("odom_report"):
+                        print("odom report!")
+                else:
+                    print(f"chk failed: {chk} vs {self.compute_chk(payload)}")
+                state = 0
 
 
 if __name__ == "__main__":
     com = Com("/dev/ttyUSB0")
     
-    while True:
-        com.send_speed_cmd(10, 12, 16)
-        break
-        time.sleep(1)   
-        
+    com.send_speed_cmd(10, 12, 16)
+    com.rcv() 
         
