f52fbc41c42a29d8309825985dbd279de34252c9
Fabien-B <fabien.bonneval@gmail.com>
2021-11-09 02:29:23 +0100

    Add HolonomicControl.

---
+++ firmware/src/HolonomicControl.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "arm_math.h"
+
+
+class HolonomicControl {
+public:
+    HolonomicControl();
+
+private:
+
+};
+
+
+
+// #ifdef __cplusplus
+// extern "C" {
+// #endif
+
+// void start_motor_control_pid(void);
+// void set_speed_setPoint(float32_t vx, float32_t vy, float32_t vtheta);
+
+// /**
+//  * Set speed setPoint from norm and direction.
+//  * speed: robot speed in mm/s
+//  * direction: speed direction in radians, [0, 2*pi]
+//  * omega: rotation speed in rad/s
+//  */
+// void set_speed_setPoint_norm_dir(float32_t speed, float32_t direction, float32_t omega);
+
+// void set_pid_gains(float32_t kp, float32_t ki, float32_t kd);
+
+// #ifdef __cplusplus
+// }
+// #endif
