9488d7daf280bcba3f9c2b37e8cf9e0d98d81d96
Fabien-B <fabien.bonneval@gmail.com>
2022-04-20 02:19:23 +0200

    Remove CMSIS ARM math.

--- firmware/cmsis_arm/files.mk
+++
@@ -1,9 +0,0 @@
-
-CSRC += $(DSPBASE)/arm_cos_f32.c
-CSRC += $(DSPBASE)/arm_sin_f32.c
-CSRC += $(DSPBASE)/arm_mat_mult_f32.c
-CSRC += $(DSPBASE)/arm_mat_add_f32.c
-CSRC += $(DSPBASE)/arm_mat_sub_f32.c
-CSRC += $(DSPBASE)/arm_mat_scale_f32.c
-CSRC += $(DSPBASE)/arm_common_tables.c
-
