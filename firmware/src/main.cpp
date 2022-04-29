d06428c2c6aaff3d799eeca4f30dcdec1b3a9845
Fabien-B <fabien.bonneval@gmail.com>
2022-04-29 14:41:04 +0200

    Initialize console before use of chprintf.

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -20,8 +20,8 @@ extern "C" {
 #include "speed_control.h"
 #include "power_check.h"
 
-ioline_t leds[] = {
-  LINE_LED_GREEN,
+  //LEDS :
+  //LINE_LED_GREEN,
   //LINE_LED_YELLOW,
   //LINE_LED_ORANGE,
   //LINE_LED_RED,
@@ -29,10 +29,6 @@ ioline_t leds[] = {
   //LINE_LED_SPI,
   //LINE_LED_UART,
   //LINE_LED_I2C,
-};
-
-
-
 
 
 static THD_WORKING_AREA(waBlinker, 304);
@@ -40,14 +36,10 @@ static void blinker (void *arg)
 {
   (void)arg;
   chRegSetThreadName("blinker");
-  
-  const int NB_LEDS = sizeof(leds)/sizeof(leds[0]);
 
   while (true) {
-    for(int i=0;i<NB_LEDS; i++) {
-      palToggleLine(leds[i]);
-      chThdSleepMilliseconds(200);
-    }
+    palToggleLine(LINE_LED_GREEN);
+    chThdSleepMilliseconds(200);
   }
 }
 
@@ -60,14 +52,14 @@ int main (void)
   initHeap();		// initialisation du "tas" pour permettre l'allocation mémoire dynamique 
 
   comm_init();
+  consoleInit();	// initialisation des objets liés au shell
 
   chThdCreateStatic(waBlinker, sizeof(waBlinker), NORMALPRIO, &blinker, NULL); // lancement du thread 
+
   start_power_check();
   start_motor_control_pid();
   start_communication();
 
-  consoleInit();	// initialisation des objets liés au shell
-  
   // cette fonction en interne fait une boucle infinie, elle ne sort jamais
   // donc tout code situé après ne sera jamais exécuté.
   consoleLaunch();  // lancement du shell
